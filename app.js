b518f5d4a6d2495beb6157875fd9515a918c5e1f
SoleSmith <fernanda.smith.ss@gmail.com>
2023-11-15 16:40:59 -0300

    change name port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const app = express ();
 const path = require ('path');
-const PORT = PROCESS-env.port || 3001;
+const PORT = process.env.port || 3001;
 app.listen(PORT,()=>console.log(`Servidor corriendo en http://localhost:${PORT}`));
 
 app.use(express.static('public'));
