c66f9042ba4443a0a00a3c2afc21dd25452333ee
SoleSmith <fernanda.smith.ss@gmail.com>
2023-11-15 13:08:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const app = express ();
+const path = require ('path');
+const PORT = PROCESS-env.port || 3001;
+app.listen(PORT,()=>console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"))
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.listen(PORT, () =>{
+    console.log('Servidor funcionando')
+});
+
